027f46ca044fc6ab3854b6d8b61f2d9b494bfaef
Alckor <alckordev@gmail.com>
2025-09-02 20:12:36 -0500

    feat(brand): :sparkles: logo and favicon

--- src/components/logo-brand.tsx
+++ src/components/logo-brand.tsx
@@ -1,19 +1,33 @@
 import { Link } from "@/i18n/navigation";
 import { cn } from "@/lib/cn";
+import Image from "next/image";
 
 export const LogoBrand = () => {
   return (
     <Link
       href="/"
       className={cn(
-        "group text-xl font-semibold transition-all duration-200",
-        "after:bg-accent-500 relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:transition-all after:duration-300 after:content-[''] hover:after:w-full",
+        "group/logo flex items-center text-2xl font-semibold transition-all duration-200",
       )}
     >
-      <span className="text-neutral-950 [.dark_&]:text-neutral-100">
-        alckor
+      <Image
+        src="/assets/iso.svg"
+        alt="Iso"
+        width={14}
+        height={20}
+        className="me-2"
+      />
+      <span
+        className={cn(
+          "after:bg-accent-500 relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:transition-all after:duration-300 after:content-['']",
+          "group-hover/logo:after:w-full hover:after:w-full",
+        )}
+      >
+        <span className="text-neutral-950 [.dark_&]:text-neutral-100">
+          alckor
+        </span>
+        <span className="text-accent-500">.dev</span>
       </span>
-      <span className="text-accent-500">.dev</span>
     </Link>
   );
 };
