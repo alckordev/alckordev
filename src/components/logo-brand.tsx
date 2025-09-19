3a3c6ca6f0886d89e0f5c0f3014960ca97bbd729
Alckor <alckordev@gmail.com>
2025-09-19 12:40:37 -0500

    feat: remove old iso svg and add new logo brand

--- src/components/logo-brand.tsx
+++ src/components/logo-brand.tsx
@@ -1,8 +1,11 @@
 import { Link } from "@/i18n/navigation";
 import { cn } from "@/lib/cn";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 
 export const LogoBrand = () => {
+  const { theme } = useTheme();
+
   return (
     <Link
       href="/"
@@ -10,24 +13,11 @@ export const LogoBrand = () => {
         "group/logo flex items-center text-2xl font-semibold transition-all duration-200",
       )}
     >
-      <Image
-        src="/assets/iso.svg"
-        alt="Iso"
-        width={14}
-        height={20}
-        className="me-2"
-      />
-      <span
-        className={cn(
-          "after:bg-accent-500 relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:transition-all after:duration-300 after:content-['']",
-          "group-hover/logo:after:w-full hover:after:w-full",
-        )}
-      >
-        <span className="text-neutral-950 [.dark_&]:text-neutral-100">
-          alckor
-        </span>
-        <span className="text-accent-500">dev</span>
-      </span>
+      {theme === "dark" ? (
+        <Image src="/assets/logo-dark.svg" alt="Iso" width={125} height={30} />
+      ) : (
+        <Image src="/assets/logo-light.svg" alt="Iso" width={125} height={30} />
+      )}
     </Link>
   );
 };
